5c3e251e4f6a03f7ebda326162416b9913d207e7
rohan0005 <khadkarohan143@gmail.com>
2023-08-13 19:46:14 +0545

    created explore page

--- craftify/core/urls.py
+++ craftify/core/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('explore', views.explore, name='explore'),
+    
+    
    
     
 ]
--- craftify/craftify/settings.py
+++ craftify/craftify/settings.py
@@ -113,12 +113,29 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS =[
+    BASE_DIR, "static"
+]
+
+
+import os
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
